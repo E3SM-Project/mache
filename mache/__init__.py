38e662ffa1c878dd974f2aafdebb5c7577a450ca
Xylar Asay-Davis <xylarstorm@gmail.com>
2021-10-18 22:12:12 -0500

    Update to v1.1.2

--- mache/__init__.py
+++ mache/__init__.py
@@ -1,5 +1,5 @@
 from mache.machine_info import MachineInfo
 from mache.discover import discover_machine
 
-__version_info__ = (1, 1, 1)
+__version_info__ = (1, 1, 2)
 __version__ = '.'.join(str(vi) for vi in __version_info__)
