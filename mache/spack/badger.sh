1eea9e01b366f2283ac53db1065b13453fee57a0
Xylar Asay-Davis <xylarstorm@gmail.com>
2022-01-03 13:00:38 +0100

    Add proxy variables to badger scripts

---
+++ mache/spack/badger.sh
@@ -0,0 +1,6 @@
+export http_proxy=http://proxyout.lanl.gov:8080/
+export https_proxy=http://proxyout.lanl.gov:8080/
+export ftp_proxy=http://proxyout.lanl.gov:8080
+export HTTP_PROXY=http://proxyout.lanl.gov:8080
+export HTTPS_PROXY=http://proxyout.lanl.gov:8080
+export FTP_PROXY=http://proxyout.lanl.gov:8080
