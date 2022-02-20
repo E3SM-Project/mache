f53bfe3135f3a088e0352a9426cec7014e61c8d6
Xylar Asay-Davis <xylarstorm@gmail.com>
2022-02-19 21:28:00 -0700

    Add an entry point to sync diags from LCRC

---
+++ mache/sync/__init__.py
@@ -0,0 +1,36 @@
+import sys
+import argparse
+
+from mache.sync import diags
+
+def main():
+    """
+    Defines the ``mache sync`` command
+    """
+
+    parser = argparse.ArgumentParser(
+        description="Perform synchronization between supported machines",
+        usage='''
+    mache sync <command> [<args>]
+    The available mache commands are:
+        diags    Synchronize diagnostics files between supported machines
+     To get help on an individual command, run:
+        mache sync <command> --help
+        ''')
+
+    parser.add_argument('command', help='command to run')
+    if len(sys.argv) == 2:
+        parser.print_help()
+        sys.exit(0)
+
+    args = parser.parse_args(sys.argv[2:3])
+
+    commands = {'diags': diags.main}
+
+    if args.command not in commands:
+        print('Unrecognized command {}'.format(args.command))
+        parser.print_help()
+        exit(1)
+
+    # call the function associated with the requested command
+    commands[args.command]()
---
+++ mache/sync/diags.py
@@ -0,0 +1,128 @@
+import sys
+import argparse
+import subprocess
+
+from mache.machine_info import MachineInfo
+from mache.permissions import update_permissions
+
+
+def sync_diags(other, direction='to', machine=None, username=None):
+    """
+    Synchronize diagnostics files between supported machines
+
+    Parameters
+    ----------
+    other : str
+        The other machine to sync to or from
+    direction : {'to', 'from'}, optional
+        The direction to sync (to or from the other machine).
+    machine : str, optional
+        The name of this machine.  If not provided, it will be detected
+        automatically
+    username : str, optional
+        The username to use on the other machine, if not from ``.ssh/config``
+    """
+    if direction not in ['to', 'from']:
+        raise ValueError('The direction must be one of "to" or "from"')
+
+    machine_info = MachineInfo(machine=machine)
+    machine = machine_info.machine
+
+    lcrc_machines = ['anvil', 'chrysalis']
+    if direction == 'to' and machine not in lcrc_machines:
+        raise ValueError(f'You can only sync diagnostics to another machine '
+                         f'from an LCRC machine: {", ".join(lcrc_machines)}')
+    if direction == 'from' and other not in lcrc_machines:
+        raise ValueError(f'You can only sync diagnostics from an LCRC '
+                         f'machine: {", ".join(lcrc_machines)}')
+    machine_config = machine_info.config
+    other_info = MachineInfo(machine=other)
+    other_config = other_info.config
+
+    hostname = other_config.get('sync', 'hostname')
+    if other_config.has_option('sync', 'tunnel_hostname'):
+        tunnel = other_config.get('sync', 'tunnel_hostname')
+    else:
+        tunnel = None
+
+    if direction == 'to' and (machine != other or tunnel is None):
+        raise ValueError('You can only use "mache sync diags to ..." to '
+                         'sync diags between directories on an LCRC machine\n'
+                         'or to a machine with a tunnel.  Other machines '
+                         'should only use "mache sync diags from ..." so\n'
+                         'permissions can be updated.')
+
+    if direction == 'from':
+        source_config = other_config
+        dest_config = machine_config
+    else:
+        source_config = machine_config
+        dest_config = other_config
+
+    public_diags = source_config.get('sync', 'public_diags')
+    private_diags = source_config.get('sync', 'private_diags')
+    dest_diags = dest_config.get('diagnostics', 'base_path')
+
+    if machine == other:
+        prefix = ''
+    else:
+        if username is not None:
+            prefix = f'{username}@{hostname}:'
+        else:
+            prefix = f'{hostname}:'
+
+    if direction == 'from':
+        public_diags = f'{prefix}{public_diags}'
+        private_diags = f'{prefix}{private_diags}'
+    else:
+        dest_diags = f'{prefix}{dest_diags}'
+
+    args = ['rsync', '--verbose', '--recursive', '--times', '--links',
+            '--compress', '--progress', '--update',
+            '--exclude=observations/Atm', '--no-perms', '--omit-dir-times']
+
+    if tunnel:
+        args.append(f'--rsync-path="ssh {tunnel} rsync"')
+
+    public_args = args + [public_diags, dest_diags]
+    private_args = args + [private_diags, dest_diags]
+
+    print(f'running: {" ".join(public_args)}')
+    subprocess.check_call(public_args)
+    print(f'running: {" ".join(private_args)}')
+    subprocess.check_call(private_args)
+
+    if direction == 'from':
+        group = machine_config.get('diagnostics', 'group')
+        print(f'Updating permissions on {dest_diags}:')
+        update_permissions(base_paths=dest_diags, group=group,
+                           show_progress=True, group_writable=True,
+                           other_readable=True)
+        print('Done.')
+
+
+def main():
+    """
+    Defines the ``mache sync diags`` command
+    """
+    parser = argparse.ArgumentParser(
+        description="Synchronize diagnostics files between supported machines",
+        usage='''
+    mache sync diags to <other> [<args>]
+        or
+    mache sync diags from <other> [<args>]
+
+    To get help on an individual command, run:
+        mache sync <command> --help
+        ''')
+    parser.add_argument('direction', help='whether to sync "to" or "from" the '
+                                          'other machine')
+    parser.add_argument("other", help="The other machine to sync to or from")
+    parser.add_argument("-m", "--machine", dest="machine",
+                        help="The name of this machine.  If not provided, it "
+                             "will be detected automatically")
+    parser.add_argument("-u", "--username", dest="username",
+                        help="The username to use on the other machine, if "
+                             "not from .ssh/config")
+    args = parser.parse_args(sys.argv[3:])
+    sync_diags(args.other, args.direction, args.machine, args.username)
