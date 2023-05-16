0995df07f4a8cf7509ade27265911259c3111ed8
Xylar Asay-Davis <xylarstorm@gmail.com>
2023-05-16 07:45:31 -0600

    Update pm-cpu gnu modules and env. vars

--- mache/spack/pm-cpu_gnu_mpich.sh
+++ mache/spack/pm-cpu_gnu_mpich.sh
@@ -1,11 +1,19 @@
-module rm PrgEnv-gnu
-module rm PrgEnv-nvidia
-module rm cudatoolkit
-module rm craype-accel-nvidia80
-module rm craype-accel-host
-module rm perftools-base
-module rm perftools
-module rm darshan
+module rm cray-hdf5-parallel &> /dev/null
+module rm cray-netcdf-hdf5parallel &> /dev/null
+module rm cray-parallel-netcdf &> /dev/null
+module rm PrgEnv-gnu &> /dev/null
+module rm PrgEnv-intel &> /dev/null
+module rm PrgEnv-nvidia &> /dev/null
+module rm PrgEnv-cray &> /dev/null
+module rm PrgEnv-aocc &> /dev/null
+module rm intel &> /dev/null
+module rm intel-oneapi &> /dev/null
+module rm cudatoolkit &> /dev/null
+module rm craype-accel-nvidia80 &> /dev/null
+module rm craype-accel-host &> /dev/null
+module rm perftools-base &> /dev/null
+module rm perftools &> /dev/null
+module rm darshan &> /dev/null
 
 module load PrgEnv-gnu/8.3.3
 module load gcc/11.2.0
@@ -14,22 +22,22 @@ module load craype-accel-host
 module load cray-libsci/23.02.1.1
 {% endif %}
 module load craype/2.7.19
-module rm cray-mpich
+module rm cray-mpich &> /dev/null
 module load libfabric/1.15.2.0
 module load cray-mpich/8.1.24
 {% if e3sm_hdf5_netcdf %}
-module rm cray-hdf5-parallel
-module rm cray-netcdf-hdf5parallel
-module rm cray-parallel-netcdf
+module rm cray-hdf5-parallel &> /dev/null
+module rm cray-netcdf-hdf5parallel &> /dev/null
+module rm cray-parallel-netcdf &> /dev/null
 module load cray-hdf5-parallel/1.12.2.3
 module load cray-netcdf-hdf5parallel/4.9.0.3
 module load cray-parallel-netcdf/1.12.3.3
 {% endif %}
 
 {% if e3sm_hdf5_netcdf %}
-export NETCDF_C_PATH=$(dirname $(dirname $(which nc-config)))
-export NETCDF_FORTRAN_PATH=$(dirname $(dirname $(which nf-config)))
-export PNETCDF_PATH=$(dirname $(dirname $(which pnetcdf_version)))
+export NETCDF_C_PATH=$CRAY_NETCDF_HDF5PARALLEL_PREFIX
+export NETCDF_FORTRAN_PATH=$CRAY_NETCDF_HDF5PARALLEL_PREFIX
+export PNETCDF_PATH=$CRAY_PARALLEL_NETCDF_PREFIX
 {% endif %}
 export MPICH_ENV_DISPLAY=1
 export MPICH_VERSION_DISPLAY=1
