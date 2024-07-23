ee072e6604357eaab1bbb6f0078633df99a44877
Xylar Asay-Davis <xylarstorm@gmail.com>
2024-07-23 10:19:34 -0500

    Omit OMP* environment variables with gnu on chrysalis
    They are causing very bad slowdown in the Polaris pr test suite.

--- mache/spack/chrysalis_gnu_openmpi.sh
+++ mache/spack/chrysalis_gnu_openmpi.sh
@@ -1,4 +1,5 @@
 export OMPI_MCA_sharedfp="^lockedfile,individual"
 export UCX_TLS="^xpmem"
-export OMP_STACKSIZE=128M
-export OMP_PLACES=cores
+## purposefully omitting OMP variables that slow down MPAS-Ocean runs
+#export OMP_STACKSIZE=128M
+#export OMP_PLACES=cores
