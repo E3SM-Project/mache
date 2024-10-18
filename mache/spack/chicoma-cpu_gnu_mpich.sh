76921134b2acdb40fb6841c50db00a9e36106224
Althea Denlinger <altheaden@unm.edu>
2024-10-18 12:20:05 -0500

    Update `chicoma-cpu_gnu_mpich` files

--- mache/spack/chicoma-cpu_gnu_mpich.sh
+++ mache/spack/chicoma-cpu_gnu_mpich.sh
@@ -5,23 +5,23 @@ export HTTP_PROXY=http://proxyout.lanl.gov:8080
 export HTTPS_PROXY=http://proxyout.lanl.gov:8080
 export FTP_PROXY=http://proxyout.lanl.gov:8080
 
-source /usr/share/lmod/8.3.1/init/sh
+source /usr/share/lmod/lmod/init/sh
 
+module rm craype-accel-nvidia80
+module rm craype-accel-host
+module rm cray-libsci
 module rm PrgEnv-gnu
 module rm PrgEnv-nvidia
 module rm PrgEnv-cray
 module rm PrgEnv-aocc
-module rm craype-accel-nvidia80
-module rm craype-accel-host
 
-module load PrgEnv-gnu/8.4.0
+module load PrgEnv-gnu/8.5.0
 module load gcc/12.2.0
 module load craype-accel-host
 {% if e3sm_lapack %}
 module load cray-libsci
 {% endif %}
 module load craype
-module load libfabric/1.15.2.0
 module load cray-mpich/8.1.26
 {% if e3sm_hdf5_netcdf %}
 module rm cray-hdf5-parallel
@@ -31,6 +31,7 @@ module load cray-hdf5-parallel/1.12.2.3
 module load cray-netcdf-hdf5parallel/4.9.0.3
 module load cray-parallel-netcdf/1.12.3.3
 {% endif %}
+module load cmake/3.27.7
 
 export MPICH_ENV_DISPLAY=1
 export MPICH_VERSION_DISPLAY=1
@@ -44,4 +45,4 @@ export PNETCDF_HINTS="romio_ds_write=disable;romio_ds_read=disable;romio_cb_writ
 export FI_CXI_RX_MATCH_MODE=software
 export MPICH_COLL_SYNC=MPI_Bcast
 
-export LD_LIBRARY_PATH=$CRAY_LD_LIBRARY_PATH:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH="/opt/cray/pe/gcc/12.2.0/snos/lib64:${CRAY_LD_LIBRARY_PATH}:${LD_LIBRARY_PATH}"
