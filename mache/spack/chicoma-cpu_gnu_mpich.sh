a9f48e0d42073bc073b9f9fc5da59491f86d84f2
Xylar Asay-Davis <xylarstorm@gmail.com>
2024-10-19 15:21:34 -0600

    Remove and load modules together
    At least to the extent possible.

--- mache/spack/chicoma-cpu_gnu_mpich.sh
+++ mache/spack/chicoma-cpu_gnu_mpich.sh
@@ -7,43 +7,44 @@ export FTP_PROXY=http://proxyout.lanl.gov:8080
 
 source /usr/share/lmod/lmod/init/sh
 
-module rm cray-hdf5-parallel
-module rm cray-netcdf-hdf5parallel
-module rm cray-parallel-netcdf
-module rm cray-netcdf
-module rm cray-hdf5
-module rm intel
-module rm intel-oneapi
-module rm nvidia
-module rm aocc
-module rm cudatoolkit
-module rm climate-utils
-module rm cray-libsci
-module rm craype-accel-nvidia80
-module rm craype-accel-host
-module rm perftools-base
-module rm perftools
-module rm darshan
-module rm PrgEnv-gnu
-module rm PrgEnv-intel
-module rm PrgEnv-nvidia
-module rm PrgEnv-cray
-module rm PrgEnv-aocc
+module rm cray-hdf5-parallel \
+          cray-netcdf-hdf5parallel \
+          cray-parallel-netcdf \
+          cray-netcdf \
+          cray-hdf5 \
+          intel \
+          intel-oneapi \
+          nvidia \
+          aocc \
+          cudatoolkit \
+          climate-utils \
+          cray-libsci \
+          craype \
+          craype-accel-nvidia80 \
+          craype-accel-host \
+          perftools-base \
+          perftools \
+          darshan \
+          PrgEnv-gnu \
+          PrgEnv-intel \
+          PrgEnv-nvidia \
+          PrgEnv-cray \
+          PrgEnv-aocc
 
-module load PrgEnv-gnu/8.5.0
-module load gcc/12.2.0
-module load craype-accel-host
+module load PrgEnv-gnu/8.5.0 \
+            gcc/12.2.0 \
+            craype-accel-host \
+            cray-mpich/8.1.26 \
+            craype \
+            cmake/3.27.7
 {% if e3sm_lapack %}
-module load cray-libsci
+module load cray-libsci/23.05.1.4
 {% endif %}
-module load craype
-module load cray-mpich/8.1.26
 {% if e3sm_hdf5_netcdf %}
-module load cray-hdf5-parallel/1.12.2.3
-module load cray-netcdf-hdf5parallel/4.9.0.3
-module load cray-parallel-netcdf/1.12.3.3
+module load cray-hdf5-parallel/1.12.2.3 \
+            cray-netcdf-hdf5parallel/4.9.0.3 \
+            cray-parallel-netcdf/1.12.3.3
 {% endif %}
-module load cmake/3.27.7
 
 export MPICH_ENV_DISPLAY=1
 export MPICH_VERSION_DISPLAY=1
