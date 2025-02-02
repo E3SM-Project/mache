894af8881a0d7c6b7c2b6c3d435fe3c17ced2071
Xylar Asay-Davis <xylarstorm@gmail.com>
2025-02-02 03:37:29 -0600

    Update docs and add imports for convenience

--- mache/spack/__init__.py
+++ mache/spack/__init__.py
@@ -6,6 +6,12 @@ import yaml
 from jinja2 import Template
 
 from mache.machine_info import MachineInfo, discover_machine
+from mache.spack.config_machines import (  # noqa: F401
+    config_to_shell_script,
+    extract_machine_config,
+    extract_spack_from_config_machines,
+)
+from mache.spack.list import list_machine_compiler_mpilib  # noqa: F401
 from mache.version import __version__
 
 
