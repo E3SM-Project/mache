9b39c08f8390b21015a53ad0e76fd26080148dce
Althea Denlinger <altheaden@unm.edu>
2025-03-25 08:00:10 -0700

    Re-format function calls

--- utils/update_cime_machine_config.py
+++ utils/update_cime_machine_config.py
@@ -13,25 +13,29 @@ def main():
     Main function to download the XML file, get supported machines, and extract
     them, then compare the machine configurations between the old and new XML.
     """
-    url = ('https://raw.githubusercontent.com/E3SM-Project/E3SM/refs/heads/'
-           'master/cime_config/machines/config_machines.xml')
+    url = (
+        'https://raw.githubusercontent.com/E3SM-Project/E3SM/refs/heads/'
+        'master/cime_config/machines/config_machines.xml'
+    )
     new_filename = 'new_config_machines.xml'
     download_file(url, new_filename)
     machines = get_supported_machines()
-    extract_supported_machines(new_filename,
-                               'new_config_supported_machines.xml',
-                               machines)
+    extract_supported_machines(
+        new_filename, 'new_config_supported_machines.xml', machines
+    )
 
     old_filename = 'mache/cime_machine_config/config_machines.xml'
-    extract_supported_machines(old_filename,
-                               'old_config_supported_machines.xml',
-                               machines)
+    extract_supported_machines(
+        old_filename, 'old_config_supported_machines.xml', machines
+    )
 
     for machine in machines:
-        compare_machine_configs('old_config_supported_machines.xml',
-                                'new_config_supported_machines.xml',
-                                machine)
+        compare_machine_configs(
+            'old_config_supported_machines.xml',
+            'new_config_supported_machines.xml',
+            machine,
+        )
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
