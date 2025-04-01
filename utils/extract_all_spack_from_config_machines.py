fb36f694be85964b6ddb4fd96210e2b81e37715c
Althea Denlinger <theabecca@gmail.com>
2025-04-01 12:17:52 -0500

    Fix formatting in utils script

--- utils/extract_all_spack_from_config_machines.py
+++ utils/extract_all_spack_from_config_machines.py
@@ -17,8 +17,9 @@ def main():
         print(f'Extracting {machine}, {compiler}, {mpilib}')
         for shell in ['sh', 'csh']:
             filename = f'{directory}/{machine}_{compiler}_{mpilib}.{shell}'
-            extract_spack_from_config_machines(machine, compiler, mpilib,
-                                               shell, filename)
+            extract_spack_from_config_machines(
+                machine, compiler, mpilib, shell, filename
+            )
 
 
 if __name__ == '__main__':
