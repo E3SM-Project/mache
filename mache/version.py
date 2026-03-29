8aa6923b9b1003b668bd41ebdcd923852acf03cb
Xylar Asay-Davis <xylarstorm@gmail.com>
2026-03-29 16:56:12 +0200

    Update to 3.3.0rc2

--- mache/version.py
+++ mache/version.py
@@ -1,2 +1,3 @@
-__version_info__ = (3, 2, 0)
-__version__ = '.'.join(str(vi) for vi in __version_info__)
+__version_info__ = (3, 3, 0)
+# __version__ = '.'.join(str(vi) for vi in __version_info__)
+__version__ = '3.3.0rc2'
